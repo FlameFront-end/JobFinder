edde9ca74cdb5ed6f14ca266dd33243c08dc319d
Flame <5017_30@mail.ru>
2023-06-08 23:13:03 +0300

    Updates

--- src/store/root-reducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { positionsReducer } from "./positions/positions-reducer";
-import { filterReducer } from "./filters/filter-reducer";
-
-export const rootReducer = combineReducers({
-  positions: positionsReducer,
-  filters: filterReducer,
-});
